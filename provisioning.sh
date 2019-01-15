83fd5cfa2cddc81440d707e9de3d88cd160bdb0a
Thaylo Xavier <thayloxavier@gmail.com>
2019-01-14 23:45:49 -0200

    Ignoring build

--- provisioning.sh
+++ provisioning.sh
@@ -2,5 +2,9 @@
 sudo apt-get update -y &&\
 sudo apt-get install libgl1-mesa-dev -y &&\
 sudo apt install freeglut3-dev -y &&\
-sudo apt autoremove -y
+sudo apt install cmake -y
 gradle wrapper
+cd 3rdparty
+git clone https://github.com/google/googletest.git googletest-master
+cd googletest-master
+mkdir build
